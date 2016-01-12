c1379486baea17a8a4ec48210a37e221afda9c81
rfraposa <rich@hortonworks.com>
2016-01-12 09:01:44 -0700

    Updated script to use 2.2

--- ambari_single_node.sh
+++ ambari_single_node.sh
@@ -5,6 +5,6 @@ BLUEPRINT_BASE=$1
 
 #Start the Ambari Server 
 echo "Starting Namenode/Ambari Server..."
-docker run --privileged=true -d --dns 8.8.8.8 -p 8080:8080 -p 8440:8440 -p 8441:8441 -p 50070:50070 -p 8020:8020 -e AMBARI_SERVER=namenode -e BLUEPRINT_BASE=${BLUEPRINT_BASE} --name namenode -h namenode -i -t hwxu/ambari_2.1_server_node
+docker run --privileged=true -d --dns 8.8.8.8 -p 8080:8080 -p 8440:8440 -p 8441:8441 -p 50070:50070 -p 8020:8020 -e AMBARI_SERVER=namenode -e BLUEPRINT_BASE=${BLUEPRINT_BASE} --name namenode -h namenode -i -t hwxu/ambari_2.2_server_node
 IP_namenode=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" namenode)
 echo "Namenode/Ambari Server started at $IP_namenode"
